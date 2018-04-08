ddca10182d214773b4b13ab3c4c56c2393bd3f5b
Rimil Dey <rimildeyjsr@gmail.com>
2018-04-08 21:22:35 +0530

    initialization for table view cell

--- Luna/PhaseTableViewCell.swift
+++ Luna/PhaseTableViewCell.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class PhaseTableViewCell: UITableViewCell {
+    
+    @IBOutlet var iconLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        self.iconLabel.text = ""
+        self.dateLabel.text = ""
+        
+        self.selectionStyle = UITableViewCellSelectionStyle.none
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
