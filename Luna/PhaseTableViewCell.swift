aa9d229e34b62999dfcc2da27e920f8b190d8ec6
Rimil Dey <rimildeyjsr@gmail.com>
2018-04-08 21:05:16 +0530

    added phase table view cell xib and swift file

---
+++ Luna/PhaseTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhaseTableViewCell.swift
+//  Luna
+//
+//  Created by Rimil Dey on 08/04/18.
+//  Copyright © 2018 Rimil Dey. All rights reserved.
+//
+
+import UIKit
+
+class PhaseTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
